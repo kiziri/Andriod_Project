40775cc903851b9afa26ebad6cfe1ea1352dce7b
kiziri <58429073+kiziri@users.noreply.github.com>
2020-11-11 01:32:46 +0900

    Add layout designs

---
+++ app/src/main/java/com/example/andriod_project/ModeSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.andriod_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_selection);
+    }
+}
