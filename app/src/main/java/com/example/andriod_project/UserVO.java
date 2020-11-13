683bdffa7db2afa414f5aaae69af94f0d0cd1fba
kiziri <58429073+kiziri@users.noreply.github.com>
2020-11-13 13:27:26 +0900

    Add UserVO file and edit code

---
+++ app/src/main/java/com/example/andriod_project/UserVO.java
@@ -0,0 +1,40 @@
+package com.example.andriod_project;
+
+public class UserVO {
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String userNickname;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+}
