6396c7fa967c8139dd7683fe9754084497c8983f
kiziri <58429073+kiziri@users.noreply.github.com>
2020-11-23 19:18:45 +0900

    Update main_home layout, activity file

--- app/src/main/java/com/example/andriod_project/MainHomeActivity.java
+++ app/src/main/java/com/example/andriod_project/MainHomeActivity.java
@@ -3,12 +3,32 @@ package com.example.andriod_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainHomeActivity extends AppCompatActivity {
+
+    TextView userNicknameTxtView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
+
+    }
+
+    // 버튼 이벤트 리스터 구현부
+    public void gameModeSelectSystem(View view) {
+
+        switch (view.getId()) {
+            case R.id.storyBtn :
+                break;
+            case R.id.challengeBtn :
+                break;
+            case R.id.studyBtn :
+                break;
+        }
     }
 }
