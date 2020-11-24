10b2c8a88f497184162983a7a3490150030536a9
kiziri <58429073+kiziri@users.noreply.github.com>
2020-11-25 03:05:04 +0900

    Update mainHomeActivity for userInfo popupWindow

--- app/src/main/java/com/example/andriod_project/views/MainHomeActivity.java
+++ app/src/main/java/com/example/andriod_project/views/MainHomeActivity.java
@@ -9,6 +9,7 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -21,8 +22,9 @@ import retrofit2.Call;
 
 public class MainHomeActivity extends AppCompatActivity {
 
-    TextView userNicknameTxtView;
+    TextView userNicknameTxtView, nicknameTxtView, idTxtView, nameTxtView, rankPointTxt, solveProblemTxt, correctProblemTxt;
     ImageView profileImgView, RankImgView;
+    Button userInfoCloseBtn;
 
     ConstraintLayout popupConstraint;
     PopupWindow userInfoPopup;
@@ -38,11 +40,20 @@ public class MainHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
 
-        userNicknameTxtView = findViewById(R.id.userNicknameTxtView);
+        userNicknameTxtView = findViewById(R.id.userNicknameTxtView);   // 화면 상단 회원 닉네임 텍스트뷰 아이디
 
+        // 팝업 윈도우 정의 및 구현
         popupConstraint = findViewById(R.id.mainHomeLayout);
         popupView = View.inflate(this, R.layout.popup_userinfo, null);
         userInfoPopup = new PopupWindow(popupView, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT, true);
+
+        // 유저 정보 팝업 윈도우 화면 텍스트뷰 아이디 정의
+        nicknameTxtView = popupView.findViewById(R.id.nicknameTxtView);
+        idTxtView = popupView.findViewById(R.id.idTxtView);
+        nameTxtView = popupView.findViewById(R.id.nameTxtView);
+        rankPointTxt = popupView.findViewById(R.id.rankPointTxt);
+        solveProblemTxt = popupView.findViewById(R.id.solveProblemTxt);
+        correctProblemTxt = popupView.findViewById(R.id.correctProblemTxt);
 
 
         // 로그인 화면에서 로그인을 하며 MySQL에서 검색하여 받아온 로그인한 회원의
@@ -59,6 +70,14 @@ public class MainHomeActivity extends AppCompatActivity {
 
         // 로그인한 유저의 닉네임을 메인 화면의 닉네임 부분에 세팅
         userNicknameTxtView.setText(userNickname);
+
+        // 유저 정보 팝업 윈도우에 정보 세팅
+        nicknameTxtView.setText(userNickname);
+        idTxtView.setText(userId);
+        nameTxtView.setText(userName);
+        rankPointTxt.setText(userRankPoint+"점");
+        solveProblemTxt.setText(userSolveProblem+"개");
+        correctProblemTxt.setText(userCorrectProblem+"개");
     }
 
     // 버튼 이벤트 리스터 구현부
@@ -66,10 +85,19 @@ public class MainHomeActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.storyBtn :
+                intent = new Intent(MainHomeActivity.this, StoryModeActivity.class);
+                intent.putExtra("userName", userName);
+                intent.putExtra("userNickname", userNickname);
+                intent.putExtra("userRankPoint", userRankPoint);
+                intent.putExtra("userSolveProblem", userSolveProblem);
+                intent.putExtra("userCorrectProblem", userCorrectProblem);
+                System.out.println("---------------------\n");
                 break;
             case R.id.challengeBtn :
+
                 break;
             case R.id.studyBtn :
+
                 break;
             case R.id.userNicknameTxtView :
             case R.id.profileImgVIew :
